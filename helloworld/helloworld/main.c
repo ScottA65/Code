a8c1dc4e6bf24da3508a8dd53ccba07e7ed22beb
@scottalwine <scottalwine@yahoo.com>
2014-02-13 23:33:21 -0500

    test commit #1 from Xcode

--- helloworld/helloworld/main.c
+++ helloworld/helloworld/main.c
@@ -11,8 +11,27 @@
 int main(int argc, const char * argv[])
 {
 
+    int i;
+    double factorial = 1;
+    
+    for (i=1; i<=65; i++)
+    {
+        factorial *= i;
+        printf("%d! = %0.0f\n", i, factorial);
+    }
+    // printf("10! is %d.\n", factorial);
+    
     // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    // printf("Hello, World!\n");
+    
+    if(factorial == 3628800)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+    // return 0;
 }
 
